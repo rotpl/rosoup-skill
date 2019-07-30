4878b4c35b6bb58d4dee6ddddad01577d89c90d8
nwilming <nwilming@rosen-group.com>
2019-07-30 15:37:56 +0100

    First lettersoup version

--- __init__.py
+++ __init__.py
@@ -1,19 +1,27 @@
 from mycroft import MycroftSkill, intent_file_handler
+import pickle
+import cryptography
 
 
 class Rosoup(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        with open('/home/pi/fernet_key.txt', 'r', encoding='utf-8') as k:
+            key = k.read()
+        f = cryptography.Fernet(key)
+        with open('/opt/mycroft/skills/rosoup-skill/enc_soup.pickle', 'r',
+                encoding='utf-8') as k:
+            token = f.decrypt(k.read().encode('utf-8'))
+        self.soup = pickle.loads(token)
 
     @intent_file_handler('rosoup.intent')
     def handle_rosoup(self, message):
         abrv = message.data.get('abrv')
-
+        response = self.soup[abrv]
         self.speak_dialog('rosoup', data={
-            'abrv': abrv
+            'abrv': abrv, 'response': response
         })
 
 
 def create_skill():
     return Rosoup()
-
